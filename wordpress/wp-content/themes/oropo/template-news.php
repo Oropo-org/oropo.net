0414a744197495643746eaa50990a168c04328e7
Ben Immanuel <ben.immanuel@aistemos.com>
2015-06-15 12:43:32 +0100

    added pdfs with pdf extension, and templates

---
+++ wordpress/wp-content/themes/oropo/template-news.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: News
+*/
+header("Location: /oropo_news_20150615.pdf");
+die();
---
+++ wordpress/wp-content/themes/oropo/template-report.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: Report
+*/
+header("Location: /oropo_report_20150615.pdf");
+die();
